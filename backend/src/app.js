0b5b582db8e666fc08bd344571275f6cc206ab78
Mahesh M Nimkar <m.nimkar070@gmail.com>
2025-02-07 01:49:46 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const apiRoutes = require('./routes/api');
+const express = require("express");
+const cors = require("cors");
+const apiRoutes = require("./routes/api");
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+// Enable CORS for all origins
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
+
+// Allow preflight requests for all routes
 app.options("*", cors());
 
+// Middleware
+app.use(express.json());
 
 // Routes
-app.use('/api', apiRoutes);
-
+app.use("/api", apiRoutes);
 
 module.exports = app;
